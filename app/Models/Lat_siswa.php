a96e54e198e39a28f03c5d904d5ef24c17f0cbb7
Siti Zahra Azizah <114201606+stzahraPPLGA@users.noreply.github.com>
2023-04-06 11:38:01 +0800

    Add files via upload

---
+++ app/Models/Lat_siswa.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lat_siswa extends Model
+{
+    use HasFactory;
+    protected $table = 'lat_siswa';
+}
