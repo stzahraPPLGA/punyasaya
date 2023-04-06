67e8cfe7330c9226a1f63d5cd429ce7de99e7e46
Siti Zahra Azizah <114201606+stzahraPPLGA@users.noreply.github.com>
2023-04-06 11:38:36 +0800

    Add files via upload

---
+++ database/migrations/2023_04_06_011948_create_warga_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('warga', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('nik')->nulltable();
+            $table->string('no_kk')->nulltable();
+            $table->enum('jenis_kelamin', ['L','P']);
+            $table->text('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('warga');
+    }
+};
